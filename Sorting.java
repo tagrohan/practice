f4ad3c2e650b6be98bb1e877c2b841114c8c8e8d
tagrohan <tagrohan@gmail.com>
2021-04-12 05:14:16 +0530

    insertion added with changes

--- Sorting.java
+++ Sorting.java
@@ -2,43 +2,56 @@
 import java.util.Arrays;
 
 public class Sorting {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        // bubbleSort(new int[] { 3, 46, 6, 5, 45, 13, 42, 23, 2, 1 });
-        
-
-    }
+      insertionSort(new int[]{3, 46, 6,6, 5, 45, 13, 42, 23, 2, 1});
 
 
+   }
 
-    private static void bubbleSort(int[] arr) {
 
-        for (int i = 1; i <= arr.length; i++) {
-            for (int j = 0; j < arr.length - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+   // insertion working fine
+   private static void insertionSort(int[] arr) {
+      for (int i = 1; i < arr.length; i++) {
+         int key = arr[i];
+         int j;
+         for (j = i - 1; j > -1 && arr[j] > key; j--) {
+            arr[j + 1] = arr[j];
+         }
+         arr[j + 1] = key;
+      }
+      System.out.println(Arrays.toString(arr));
+   }
+
+
+   private static void bubbleSort(int[] arr) {
+
+      for (int i = 1; i <= arr.length; i++) {
+         for (int j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+               int temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
             }
-        }
-        System.out.println(Arrays.toString(arr));
-    }
+         }
+      }
+      System.out.println(Arrays.toString(arr));
+   }
 
-    private static void selectionSort(int[] arr) {
+   private static void selectionSort(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    // swapping here
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+               // swapping here
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
             }
+         }
 
-        }
-        System.out.println(Arrays.toString(arr));
-    }
+      }
+      System.out.println(Arrays.toString(arr));
+   }
 
 }
