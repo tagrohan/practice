8d4a131f98010a874e51e1366b0709746bda9205
tagrohan <tagrohan@gmail.com>
2021-04-15 04:01:55 +0530

    string added

--- com/practice/WorthConsidering.java
+++ com/practice/WorthConsidering.java
@@ -1,10 +1,36 @@
 package com.practice;
 
+import java.util.Locale;
+
 public class WorthConsidering {
 
    public static void main(String[] args) {
-      System.out.println(palindrome("0p"));
+      System.out.println(largeString("ma name is roha singh"));
    }
+
+
+   private static int largeString(String str) {
+      str = str + " ";
+      int max = 0, counter = 0;
+      int cIndex = 0, index = 0;
+
+      for (int i = 0; i < str.length(); i++) {
+         if (str.charAt(i) != ' ') {
+            counter += 1;
+         } else {
+            if (max < counter) {
+               max = counter;
+               index = cIndex;
+            }
+            cIndex = i + 1;
+            counter = 0;
+         }
+
+      }
+      System.out.println(index + "->" + max);
+      return max;
+   }
+
 
    // working but not for numeric (learn regex)
    private static int palindrome(String str) {
