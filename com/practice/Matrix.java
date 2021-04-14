62e09996b9dc05dd4fa5c97f5b81ae12276adce9
tagrohan <tagrohan@gmail.com>
2021-04-14 18:41:50 +0530

    transope added

--- com/practice/Matrix.java
+++ com/practice/Matrix.java
@@ -1,29 +1,37 @@
 package com.practice;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
 
 public class Matrix {
    public static void main(String[] args) {
-      matrix2();
-   }
-
-   private static void matrix2(){
-      String str = "rohan singh";
-      String ro = str.replaceFirst("ro", "");
-      boolean s = str.contains("s");
-      int s1 = str.indexOf(" ");
-      char arr[] = str.toCharArray();
-      Arrays.sort(arr);
-      Collections.sort(List.of(arr.clone()), Collections.reverseOrder());
+      int[][] arr = new int[3][3];
+      int count = 1;
       for (int i = 0; i < arr.length; i++) {
+         for (int j = 0; j < arr.length; j++) {
+            arr[i][j] = count;
+            count += 1;
+         }
       }
+
+      transpose(arr);
    }
 
 
-   private static void matrix(){
+   // transpose working fine
+   private static void transpose(int[][] arr) {
+      printMatrix(arr);
+      for (int i = 0; i < arr.length; i++) {
+         for (int j = i; j < arr.length; j++) {
+           int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+         }
+      }
+      System.out.println("\n");
+      printMatrix(arr);
+   }
+
+   private static void matrix() {
       int[][] arr = new int[3][3];
 
       for (int i = 0; i < arr.length; i++) {
@@ -35,9 +43,16 @@ public class Matrix {
 
       for (int i = 0; i < arr.length; i++) {
          for (int j = 0; j < arr.length; j++) {
-            System.out.print(arr[i][j]+" ");
+            System.out.print(arr[i][j] + " ");
          }
          System.out.println();
+      }
+   }
+
+   // helper method
+   private static void printMatrix(int[][] matrix) {
+      for (int[] row : matrix) {
+         System.out.println(Arrays.toString(row));
       }
    }
 
