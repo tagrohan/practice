0c38f15953572714d2a521ddf0975f5fdc598d7d
tagrohan <tagrohan@gmail.com>
2021-04-19 04:55:40 +0530

    power was added in resursion

--- com/practice/Recursion.java
+++ com/practice/Recursion.java
@@ -4,7 +4,17 @@ public class Recursion {
    // recursion practice goes from here
    public static void main(String[] args) {
 
-      System.out.println(factorial(5));
+      System.out.println(power(2, 4));
+   }
+
+
+   // power is working fine here
+   public static int power(int num, int pow) {
+      if (pow == 0) {
+         return 1;
+      }
+      int result = num * power(num, pow - 1);
+      return result;
    }
 
    // factorial using recursion
