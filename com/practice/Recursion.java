ab00544b56e19290ceb9f7bd1f883dea88e578eb
tagrohan <tagrohan@gmail.com>
2021-04-19 03:33:08 +0530

    recursion started

--- com/practice/Recursion.java
+++ com/practice/Recursion.java
@@ -4,9 +4,17 @@ public class Recursion {
    // recursion practice goes from here
    public static void main(String[] args) {
 
+      System.out.println(factorial(5));
    }
 
-   public static void staringFromHere() {
-
+   // factorial using recursion
+   public static int factorial(int num) {
+      int fac = num;
+      if (num == 0) {
+         return 1;
+      }
+      System.out.println(fac);
+      fac = fac * factorial(num - 1);
+      return fac;
    }
 }
