fdae09dd712a4d86953bf28caf7033945990a236
tagrohan <tagrohan@gmail.com>
2021-04-30 22:30:00 +0530

    searching

--- com/practice/RecursionArray.java
+++ com/practice/RecursionArray.java
@@ -1,9 +1,47 @@
 package com.practice;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RecursionArray {
     public static void main(String[] args) {
 
-        System.out.println(findFirstOnUp(new int[] { 1, 2, 5, 6, 8, 3, 9 }, 0, 8));
+        findAllIndex(new int[] { 1, 8, 5, 6, 8, 3, 8 }, 0, 8);
+        System.out.println(Arrays.toString(index.toArray()));
+    }
+
+
+    // finding all indeces of the searched element in array
+    static List<Integer> index = new ArrayList<>();
+    private static void findAllIndex(int[] arr, int i, int key) {
+        if (arr.length == i) {
+            return;
+        }
+        if (arr[i] == key) {
+            index.add(i);
+        }
+
+        findAllIndex(arr, i + 1, key);
+    }
+
+    // finding from last in array (it basically checking on at post calling)
+    private static int findLast(int[] arr, int i, int key) {
+        if (i == arr.length) {
+            return -1;
+        }
+
+        int cIndex = findLast(arr, i + 1, key);
+        if (cIndex == -1) {
+            if (arr[i] == key) {
+                return i;
+            } else {
+                return -1;
+            }
+        } else {
+            return cIndex;
+        }
     }
 
     // in this we are searching on the way up in stack
@@ -14,7 +52,7 @@ public class RecursionArray {
         if (arr[i] == key) {
             return i;
         }
-        return findFirst(arr, i, key);
+        return findFirst(arr, i + 1, key);
     }
 
     // its comparing on the way down so we have to change strategy
