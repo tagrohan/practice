75bf2ace801a3db54a68103ada4baf82adbf3bea
tagrohan <tagrohan@gmail.com>
2021-05-02 13:38:29 +0530

    array complete

--- com/practice/RecursionArray.java
+++ com/practice/RecursionArray.java
@@ -1,95 +1,110 @@
 package com.practice;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class RecursionArray {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        findAllIndex(new int[] { 1, 8, 5, 6, 8, 3, 8 }, 0, 8);
-        System.out.println(Arrays.toString(index.toArray()));
-    }
+      System.out.println(Arrays.toString(findAllIndex(new int[]{1, 8, 5, 6, 8, 3, 8}, 0, 8, 0)));
 
+   }
 
-    // finding all indeces of the searched element in array
-    static List<Integer> index = new ArrayList<>();
-    private static void findAllIndex(int[] arr, int i, int key) {
-        if (arr.length == i) {
-            return;
-        }
-        if (arr[i] == key) {
-            index.add(i);
-        }
+   // fsf = found so far (last video in array recursion pep.coding)
+   private static int[] findAllIndex(int[] arr, int i, int key, int fsf) {
 
-        findAllIndex(arr, i + 1, key);
-    }
+      if (i == arr.length) {
+         return new int[fsf];
+      }
 
-    // finding from last in array (it basically checking on at post calling)
-    private static int findLast(int[] arr, int i, int key) {
-        if (i == arr.length) {
-            return -1;
-        }
+      if (arr[i] == key) {
+         int[] whileTesting = findAllIndex(arr, i + 1, key, fsf + 1);
+         whileTesting[fsf] = i;
+         return whileTesting;
+      } else {
+         return findAllIndex(arr, i + 1, key, fsf);
+      }
+   }
 
-        int cIndex = findLast(arr, i + 1, key);
-        if (cIndex == -1) {
-            if (arr[i] == key) {
-                return i;
-            } else {
-                return -1;
-            }
-        } else {
-            return cIndex;
-        }
-    }
+   // finding all indices of the searched element in array
+   static List<Integer> index = new ArrayList<>();
 
-    // in this we are searching on the way up in stack
-    private static int findFirstOnUp(int[] arr, int i, int key) {
-        if (i == arr.length) {
-            return -1;
-        }
-        if (arr[i] == key) {
+   private static void findAllIndex(int[] arr, int i, int key) {
+      if (arr.length == i) {
+         return;
+      }
+      if (arr[i] == key) {
+         index.add(i);
+      }
+
+      findAllIndex(arr, i + 1, key);
+   }
+
+   // finding from last in array (it basically checking on at post calling)
+   private static int findLast(int[] arr, int i, int key) {
+      if (i == arr.length) {
+         return -1;
+      }
+
+      int cIndex = findLast(arr, i + 1, key);
+      if (cIndex == -1) {
+         if (arr[i] == key) {
             return i;
-        }
-        return findFirst(arr, i + 1, key);
-    }
-
-    // its comparing on the way down so we have to change strategy
-    private static int findFirst(int[] arr, int i, int key) {
-        if (i == arr.length) {
+         } else {
             return -1;
-        }
-        int cIndex = findFirst(arr, i + 1, key);
-        if (arr[i] == key) {
-            return i;
-        }
-        return cIndex;
-    }
+         }
+      } else {
+         return cIndex;
+      }
+   }
 
-    static int max = Integer.MIN_VALUE;
+   // in this we are searching on the way up in stack
+   private static int findFirstOnUp(int[] arr, int i, int key) {
+      if (i == arr.length) {
+         return -1;
+      }
+      if (arr[i] == key) {
+         return i;
+      }
+      return findFirst(arr, i + 1, key);
+   }
 
-    // max on the way up in stack
-    private static void maxInArray(int[] is, int i) {
-        if (i == is.length) {
-            System.out.println("max is " + max);
-            return;
-        }
-        max = Integer.max(is[i], max);
+   // its comparing on the way down so we have to change strategy
+   private static int findFirst(int[] arr, int i, int key) {
+      if (i == arr.length) {
+         return -1;
+      }
+      int cIndex = findFirst(arr, i + 1, key);
+      if (arr[i] == key) {
+         return i;
+      }
+      return cIndex;
+   }
 
-        maxInArray(is, i + 1);
-    }
+   static int max = Integer.MIN_VALUE;
 
-    // max on the way down in stack
-    private static int maxInArrayOnReturn(int[] is, int i) {
-        if (i == 0) {
-            return is[i];
-        }
-        int toCheck = maxInArrayOnReturn(is, i - 1);
-        if (toCheck > is[i]) {
-            return toCheck;
-        }
-        return is[i];
-    }
+   // max on the way up in stack
+   private static void maxInArray(int[] is, int i) {
+      if (i == is.length) {
+         System.out.println("max is " + max);
+         return;
+      }
+      max = Integer.max(is[i], max);
+
+      maxInArray(is, i + 1);
+   }
+
+   // max on the way down in stack
+   private static int maxInArrayOnReturn(int[] is, int i) {
+      if (i == 0) {
+         return is[i];
+      }
+      int toCheck = maxInArrayOnReturn(is, i - 1);
+      if (toCheck > is[i]) {
+         return toCheck;
+      }
+      return is[i];
+   }
 
 }
