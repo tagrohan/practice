c4abb7bc4e015a8b46ae79652235bb5dc4f5c0ad
tagrohan <tagrohan@gmail.com>
2021-05-04 00:14:33 +0530

    keypad added by myself :)

--- com/practice/RecursionInArrayList.java
+++ com/practice/RecursionInArrayList.java
@@ -1,47 +1,31 @@
 package com.practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RecursionInArrayList {
    public static void main(String[] args) {
-//      System.out.println(Arrays.toString(subsequenceTesting("abc").toArray()));
-
-      System.out.println(kadane_algo(new int[]{1,1,0,0,0,1,0,1,0,0,0}));
-
+      List<String> res = getKeypadCombination(new int[]{5, 7, 3}, 3);
+      System.out.println(res.size());
+      System.out.println(Arrays.toString(res.toArray()));
    }
 
+   // ex - 573
+   static String[] str = {"abc", "def", "ghi", "jkl", "mnop", "qrst", "uv", "wxyz", ".;", "?!"};
 
-   private static int kadane_algo(int[] arr) {
-      // kadane's algo for -ve numbers
-
-      if (arr.length == 1) {
-         return arr[0];
-      }
-      int sum = arr[0], ans = arr[0];
-      for (int i = 1; i < arr.length; i++) {
-         sum = Integer.max(sum + arr[i], arr[i]);
-         ans = Integer.max(sum, ans);
-      }
-      return ans;
-   }
-
-
-   private static List<String> subsequenceTesting(String str) {
-
-      if (str.length() == 0) {
+   private static List<String> getKeypadCombination(int[] arr, int idx) {
+      if (idx == 0) {
          return List.of("");
       }
+      String[] sub = str[arr[idx - 1] - 1].split("");
 
-      char ch = str.charAt(0);
-
-      List<String> subsequence = subsequenceTesting(str.substring(1));
-
+      List<String> toGet = getKeypadCombination(arr, idx - 1);
       List<String> list = new ArrayList<>();
-
-      for (String st : subsequence) {
-         list.add(ch + st);
-         list.add("" + st);
+      for (String val : toGet) {
+         for (String each : sub) {
+            list.add(val + each);
+         }
       }
       return list;
    }
