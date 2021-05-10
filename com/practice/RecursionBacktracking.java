63e1b671c37112f4c171add8dd8358a7012ffbae
tagrohan <tagrohan@gmail.com>
2021-05-10 18:56:27 +0530

    last of backtracking

--- com/practice/RecursionBacktracking.java
+++ com/practice/RecursionBacktracking.java
@@ -2,10 +2,61 @@ package com.practice;
 
 public class RecursionBacktracking {
    public static void main(String[] args) {
-      floodFill(new int[][]{
-              {0, 0, 0},
-              {1, 0, 1},
-              {0, 0, 0}}, 0, 0, "", new boolean[3][3]);
+      nQueensProblem(new int[][]{
+              {0, 0, 0, 0},
+              {0, 0, 0, 0},
+              {0, 0, 0, 0},
+              {0, 0, 0, 0},
+      }, 0, "");
+   }
+
+   // working fine here
+   private static void nQueensProblem(int[][] arr, int row, String asf) {
+//      nQueensProblem(new int[][]{
+//              {0, 0, 0, 0},
+//              {0, 0, 0, 0},
+//              {0, 0, 0, 0},
+//              {0, 0, 0, 0},
+//      }, 0, "");
+      if (row == arr.length) {
+         System.out.println(asf);
+         return;
+      }
+      for (int col = 0; col < arr.length; col++) {
+         if (isQueenSafe(arr, row, col)) {
+            arr[row][col] = 1;
+            nQueensProblem(arr, row + 1, asf + row + "x" + col + " ");
+            arr[row][col] = 0;
+         }
+      }
+   }
+
+   // nQueens checking all possibilities
+   private static boolean isQueenSafe(int[][] arr, int row, int col) {
+      for (int i = row - 1; i >= 0; i--) {
+         if (arr[i][col] == 1) {
+            return false;
+         }
+      }
+      for (int i = col - 1; i >= 0; i--) {
+         if (arr[row][i] == 1) {
+            return false;
+         }
+      }
+
+      for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+         if (arr[i][j] == 1) {
+            return false;
+         }
+      }
+
+      for (int i = row - 1, j = col + 1; i >= 0 && j < arr.length; i--, j++) {
+         if (arr[i][j] == 1) {
+            return false;
+         }
+      }
+      return true;
+
    }
 
 
@@ -13,7 +64,7 @@ public class RecursionBacktracking {
 //      floodFill(new int[][]{
 //              {0, 0, 0},
 //              {1, 0, 1},
-//              {0, 0, 0}}, 0, 0, "", new boolean[3][3]); path here is (R D D R)
+//              {0, 0, 0}}, 0, 0, "", new boolean[3][3]); path here is (R D D R)e
       if (r < 0 || r > maze.length - 1 || c < 0 || c > maze[0].length - 1 || maze[r][c] == 1
               || visited[r][c]) {
          return;
