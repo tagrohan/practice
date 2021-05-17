1e2388105c677427d33a72a3ba2cd6ed70c9eb43
tagrohan <tagrohan@gmail.com>
2021-05-17 06:02:26 +0530

    check bracket and balance bracket done

--- com/practice/stack/StackPep.java
+++ com/practice/stack/StackPep.java
@@ -7,6 +7,41 @@ public class StackPep {
 
    }
 
+   private static boolean balanceBrackets(String regex) {
+//      System.out.println(balanceBrackets("[(a + b) + {(c + d) * (e / f)}]"));
+      Stack<Character> stack = new Stack<>();
+      for (int i = 0; i < regex.length(); i++) {
+         char ch = regex.charAt(i);
+         if (returnOpening(ch) != ' ') {
+            if (stack.peek() != returnOpening(ch)) {
+               return false;
+            }
+            while (stack.peek() != returnOpening(ch)) {
+               stack.pop();
+            }
+            stack.pop();
+         }
+         if (ch == '(' || ch == '{' || ch == '[') {
+            stack.push(ch);
+         }
+      }
+      return stack.isEmpty();
+   }
+
+   // associate with balanceBrackets();
+   private static Character returnOpening(Character ch) {
+      switch (ch) {
+         case ')':
+            return '(';
+         case '}':
+            return '{';
+         case ']':
+            return '[';
+      }
+      return ' ';
+   }
+
+
    // working fine not fully validated
    private static boolean duplicateBrackets(String regex) {
 //      System.out.println(duplicateBrackets("(a+b)+((c+d))"));
@@ -23,7 +58,8 @@ public class StackPep {
             }
             stack.pop();
          } else {
-            stack.push(ch);
+            if (ch != ' ')
+               stack.push(ch);
          }
       }
       return false;
