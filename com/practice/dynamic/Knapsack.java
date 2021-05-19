b7b55c476bb91ee7dabbb9d52731204e0cf3bfea
tagrohan <tagrohan@gmail.com>
2021-05-20 00:39:32 +0530

    knapsack bug + unbounded fixed

--- com/practice/dynamic/Knapsack.java
+++ com/practice/dynamic/Knapsack.java
@@ -6,7 +6,8 @@ public class Knapsack {
    public static void main(String[] args) {
 
 //      System.out.println(subsetWithMinDiff(new int[]{1,2,7}));
-      System.out.println(knapsackDpTabulation(new int[]{2, 4, 6}, new int[]{8, 3, 4}, 6, 3));
+//      System.out.println(knapsackDpTabulation(new int[]{2, 4, 6}, new int[]{8, 3, 4}, 6, 3));
+
 //      System.out.println(knapsackDpMemoization(new int[]{2, 4, 6}, new int[]{8, 3, 4}, 6, 3, new int[4][7]));
 
    }
@@ -163,7 +164,7 @@ public class Knapsack {
             if (j == 0) {
                dp[i][j] = true;
             }
-            if (i > 1 && j > 1) {
+            if (i > 0 && j > 0) {
                if (arr[i - 1] <= j) {
                   dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                } else {
@@ -216,9 +217,9 @@ public class Knapsack {
    public static int knapsackDpTabulation(int[] wt, int[] val, int W, int n) {
 //      System.out.println(knapsackDpTabulation(new int[]{1, 3, 4, 5, 20, 12, 3, 2, 12, 32, 13}, new int[]{1, 4, 5, 7, 12, 2, 32, 3, 21, 23, 32}, 499, 11));
       int[][] dp = new int[n + 1][W + 1];
-      for (int i = 1; i <= n; i++) {
-         for (int j = 1; j <= W; j++) {
-            if (i - 1 == 0 && j - 1 == 0) {
+      for (int i = 0; i <= n; i++) {
+         for (int j = 0; j <= W; j++) {
+            if (i == 0 || j == 0) {
                dp[i][j] = 0;
             } else if (wt[i - 1] <= j) {
                dp[i][j] = Integer.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
--- com/practice/dynamic/Unbound.java
+++ com/practice/dynamic/Unbound.java
@@ -1,11 +1,32 @@
 package com.practice.dynamic;
 
+import java.util.Arrays;
+
 public class Unbound {
    public static void main(String[] args) {
-//      System.out.println(unboundedKnapsack(new int[]{}));
+//      System.out.println(unboundedKnapsack(new int[]{2, 4, 6}, new int[]{8, 3, 4}, 6, 3));
+      System.out.println(unboundedKnapsack(new int[]{1, 50}, new int[]{1, 30}, 100, 2));
    }
 
-   private static int unboundedKnapsack(int[] wt, int[] val, int sum) {
-      return -1;
+   // after hours working fine now : )
+   private static int unboundedKnapsack(int[] wt, int[] val, int sum, int n) {
+      int[][] dp = new int[n + 1][sum + 1];
+
+      for (int i = 0; i <= n; i++) {
+         for (int j = 0; j <= sum; j++) {
+            if (i == 0 || j == 0) {
+               dp[i][j] = 0;
+            } else if (wt[i - 1] <= j) {
+               // this is the line where magic happen as dp[i][j - wt[i - 1]] not dp[i - 1][j - wt[i - 1]]
+               dp[i][j] = Integer.max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+            } else {
+               dp[i][j] = dp[i - 1][j];
+            }
+         }
+      }
+      for (int[] var : dp) {
+         System.out.println(Arrays.toString(var));
+      }
+      return dp[n][sum];
    }
 }
