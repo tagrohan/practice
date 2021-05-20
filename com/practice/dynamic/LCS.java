6e232549edd452b6799930b8d93a982d16be8e15
tagrohan <tagrohan@gmail.com>
2021-05-20 06:09:24 +0530

    LCS started

---
+++ com/practice/dynamic/LCS.java
@@ -0,0 +1,11 @@
+package com.practice.dynamic;
+
+public class LCS {
+   public static void main(String[] args) {
+      System.out.println(longestCommonSubsequence("abcd", "bcde", 4, 4));
+   }
+
+   private static int longestCommonSubsequence(String str1, String str2, int len1, int len2) {
+      return -1;
+   }
+}
