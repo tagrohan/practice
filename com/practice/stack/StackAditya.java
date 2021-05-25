1ce88e997805d0434172f09a3455f4682c4c93bc
tagrohan <tagrohan@gmail.com>
2021-05-25 19:36:45 +0530

    (stack)largest area of rectangle in binary matrix is adde good question on stack

--- com/practice/stack/StackAditya.java
+++ com/practice/stack/StackAditya.java
@@ -5,10 +5,49 @@ import java.util.Stack;
 public class StackAditya {
    public static void main(String[] args) {
       // stack questions by Aditya verma
-      System.out.println(largestAreaHistogramPractice(new int[]{6, 2, 5, 4, 5, 1, 6}));
+      System.out.println(largestAreaHistogramPractice(new int[]{2, 4}));
+      System.out.println(largestAreaOfRectangleInMatrix(new
+              int[][]
+              {
+                      {0, 1, 1, 0},
+                      {1, 1, 1, 1},
+                      {1, 1, 1, 1},
+                      {1, 1, 0, 0}
+              }));
    }
 
+   // little complicated but working fine man ... based on largestAreaHistogram : )
+   private static int largestAreaOfRectangleInMatrix(int[][] matrix) {
+//      System.out.println(largestAreaOfRectangleInMatrix(new
+//              int[][]
+//              {
+//                      {0, 1, 1, 0},
+//                      {1, 1, 1, 1},
+//                      {1, 1, 1, 1},
+//                      {1, 1, 0, 0}
+//              }));
+      int len = matrix.length;
+      int max = 0;
+      int[] res = new int[len];
+
+      for (int[] arr : matrix) {
+         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+               res[i] = 0;
+
+            } else {
+               res[i] += arr[i];
+            }
+
+         }
+         max = Integer.max(max, largestAreaHistogramPractice(res));
+      }
+      return max;
+   }
+
+
    private static int largestAreaHistogramPractice(int[] arr) {
+//            System.out.println(largestAreaHistogram(new int[]{6, 2, 5, 4, 5, 1, 6}));
       int len = arr.length;
       int[] NSR = new int[len];
       int[] NSL = new int[len];
@@ -19,7 +58,7 @@ public class StackAditya {
             stack.pop();
          }
          if (stack.isEmpty()) {
-            NSR[i] = 7;
+            NSR[i] = len;
          } else {
             NSR[i] = stack.peek();
          }
@@ -38,6 +77,8 @@ public class StackAditya {
          }
          stack.push(i);
       }
+//      System.out.println(Arrays.toString(NSR));
+//      System.out.println(Arrays.toString(NSL));
       int max = 0;
       for (int i = 0; i < len; i++) {
          max = Integer.max((NSR[i] - NSL[i] - 1) * arr[i], max);
