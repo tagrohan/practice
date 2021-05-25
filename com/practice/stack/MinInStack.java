9addc5f468d7e852fb91e89a17dd1dd6b2d0b0da
tagrohan <tagrohan@gmail.com>
2021-05-25 20:18:08 +0530

    (stack)rain water tapping famouns question added

---
+++ com/practice/stack/MinInStack.java
@@ -0,0 +1,13 @@
+package com.practice.stack;
+
+import java.util.Stack;
+
+public class MinInStack {
+   // we have to keep minimum in stack at all time even at pop or push
+   Stack<Integer> stack = new Stack<>();
+   Stack<Integer> min = new Stack<>();
+
+   public static void main(String[] args) {
+
+   }
+}
--- com/practice/stack/StackAditya.java
+++ com/practice/stack/StackAditya.java
@@ -1,19 +1,36 @@
 package com.practice.stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class StackAditya {
    public static void main(String[] args) {
       // stack questions by Aditya verma
-      System.out.println(largestAreaHistogramPractice(new int[]{2, 4}));
-      System.out.println(largestAreaOfRectangleInMatrix(new
-              int[][]
-              {
-                      {0, 1, 1, 0},
-                      {1, 1, 1, 1},
-                      {1, 1, 1, 1},
-                      {1, 1, 0, 0}
-              }));
+      System.out.println(rainWaterTapping(new int[]{3, 0, 0, 2, 0, 4}));
+   }
+
+   // very famous question watch video or in internet to search for it
+   private static int rainWaterTapping(int[] arr) {
+//      System.out.println(rainWaterTapping(new int[]{3, 0, 0, 2, 0, 4})); sol = 10
+      int len = arr.length;
+      int[] MTR = new int[len]; // maxima to right
+      int[] MTL = new int[len]; // maxima to left
+
+      MTL[0] = arr[0];
+      for (int i = 1; i < len; i++) {
+         MTL[i] = Integer.max(MTL[i - 1], arr[i]);
+      }
+      MTR[len - 1] = arr[len - 1];
+      for (int i = len - 2; i >= 0; i--) {
+         MTR[i] = Integer.max(MTR[i + 1], arr[i]);
+      }
+      int sum = 0;
+      for (int i = 0; i < len; i++) {
+         sum += Integer.min(MTR[i], MTL[i]) - arr[i];
+      }
+      System.out.println(Arrays.toString(MTL));
+      System.out.println(Arrays.toString(MTR));
+      return sum;
    }
 
    // little complicated but working fine man ... based on largestAreaHistogram : )
