aaf10f71d42bbf9bb4db36e335a57ee4f4f74b4f
tagrohan <tagrohan@gmail.com>
2021-05-29 03:30:00 +0530

    Generic tree started 2 problem already soved

---
+++ com/practice/trees/generic_tree/Generic.java
@@ -0,0 +1,16 @@
+package com.practice.trees.generic_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generic {
+   private static class Node {
+      int data;
+      List<Node> children;
+
+      public Node(int data) {
+         this.data = data;
+         children = new ArrayList<>();
+      }
+   }
+}
---
+++ com/practice/trees/generic_tree/GenericMain.java
@@ -0,0 +1,58 @@
+package com.practice.trees.generic_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class GenericMain {
+   private static Node root;
+
+   public static class Node {
+      int data;
+      List<Node> children;
+
+      public Node(int data) {
+         this.data = data;
+         children = new ArrayList<>();
+      }
+   }
+
+   public static void main(String[] args) {
+      int[] arr = new int[]{10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120,
+              -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+      createTree(arr);
+      printTree(root);
+   }
+
+   //working fine
+   public static void printTree(Node root) {
+      System.out.print(root.data + "-> ");
+      for (Node node : root.children) {
+         System.out.print(node.data + " ");
+      }
+      System.out.println();
+      for (Node node : root.children) {
+         printTree(node);
+      }
+   }
+
+   // working fine
+   public static void createTree(int[] arr) {
+//      int[] arr = new int[]{10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120,
+//              -1,-1, 90, -1,-1, 40, 100, -1, -1, -1};
+      Stack<Node> stack = new Stack<>();
+      for (int i = 0; i < arr.length; i++) {
+         if (arr[i] == -1) {
+            stack.pop();
+         } else {
+            Node node = new Node(arr[i]);
+            if (!stack.isEmpty()) {
+               stack.peek().children.add(node);
+            } else {
+               root = node;
+            }
+            stack.push(node);
+         }
+      }
+   }
+}
