2a054dbf6e75855ac6889943dada3b056598e7f9
tagrohan <tagrohan@gmail.com>
2021-05-30 23:23:18 +0530

    BT sum, height, nodes,max ~ min , printusing stack added

--- com/practice/trees/binary/BinaryTree.java
+++ com/practice/trees/binary/BinaryTree.java
@@ -32,7 +32,66 @@ public class BinaryTree {
               null, null, null, 75, 68, null, 70, null, null, 57, null, null};
 
       createTree(arr);
-      printRecursive(root);
+      System.out.println(size(root));
+      System.out.println(sum(root));
+      System.out.println(max(root));
+      System.out.println(height(root));
+   }
+
+
+   private static int size(Node root) {
+      if (root == null) {
+         return 0;
+      }
+      int left = size(root.left);
+      int right = size(root.right);
+      return left + right + 1;
+   }
+
+   private static int sum(Node root) {
+      if (root == null) {
+         return 0;
+      }
+      int left = sum(root.left);
+      int right = sum(root.right);
+      return root.data + left + right;
+
+   }
+
+   private static int max(Node root) {
+      if (root == null) {
+         return Integer.MIN_VALUE;
+      }
+      int left = max(root.left);
+      int right = max((root.right));
+
+      return Integer.max(Integer.max(left, right), root.data);
+   }
+
+   private static int height(Node root) {
+      if (root == null) {
+         return -1; // -1m bcz we want edges, if we want nodes height it would be 0
+      }
+      int left = height((root.left));
+      int right = height((root.right));
+
+      return Integer.max(left, right) + 1;
+   }
+
+   // working fine it's preorder traversal
+   private static void printUsingStack(Node root) {
+      Stack<Node> stack = new Stack<>();
+      stack.push(root);
+      while (!stack.isEmpty()) {
+         Node node = stack.pop();
+         if (node.right != null) {
+            stack.push(node.right);
+         }
+         if (node.left != null) {
+            stack.push(node.left);
+         }
+         System.out.println(node.data);
+      }
    }
 
    private static void printRecursive(Node root) {
@@ -48,8 +107,8 @@ public class BinaryTree {
       printRecursive(root.left);
       printRecursive(root.right);
 
-
    }
+
 
    private static Node createTree(Integer[] arr) {
       Stack<Pair> stack = new Stack<>();
