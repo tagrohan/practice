fce82a98b8400b5500d0356e1dfa8f1501b1d7a8
tagrohan <tagrohan@gmail.com>
2021-06-02 23:53:04 +0530

    HM printCommon2 added

--- com/practice/hashmapnheap/Hash.java
+++ com/practice/hashmapnheap/Hash.java
@@ -6,24 +6,43 @@ import java.util.Map;
 public class Hash {
    public static void main(String[] args) {
 
-      printCommon(new int[]{1, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6}, new int[]{5, 4, 0, 2, 6, 7, 8, 9, 5, 4});
+      printCommon2(new int[]{1, 1, 2, 2, 2, 3, 5}, new int[]{1, 1, 1, 2, 2, 4, 5});
    }
 
 
-   private static void printCommon(int[] arr, int[] arr2) {
-      printCommon(new int[]{1, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6}, new int[]{5, 4, 0, 2, 6, 7, 8, 9, 5, 4});
+   // here intersection will be printed -> intersection depends on arr2 here
+   private static void printCommon2(int[] arr, int[] arr2) {
       Map<Integer, Integer> map = new HashMap<>();
-      for (int j : arr) {
-         if (!map.containsKey(j)) {
-            map.put(j, 1);
-         }
-      }
       for (int i = 0; i < arr2.length; i++) {
          if (map.containsKey(arr2[i])) {
-            if (map.get(arr2[i]) == 1) {
-               System.out.println(arr2[i]);
-            }
-            map.put(arr[i], 2);
+            map.put(arr2[i], map.get(arr2[i]) + 1);
+         } else {
+            map.put(arr2[i], 1);
+         }
+      }
+      for (int i = 0; i < arr.length; i++) {
+         if (map.containsKey(arr[i]) && map.get(arr[i]) > 0) {
+            System.out.print(arr[i] + " ");
+            map.put(arr[i], map.get(arr[i]) - 1);
+
+         }
+      }
+   }
+
+
+   // working great
+   private static void printCommon(int[] arr, int[] arr2) {
+//      printCommon(new int[]{1, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6}, new int[]{5, 4, 0, 2, 6, 7, 8, 9, 5, 4});
+      Map<Integer, Integer> map = new HashMap<>();
+      for (int i : arr) {
+         if (!map.containsKey(i)) {
+            map.put(i, 1);
+         }
+      }
+      for (int i : arr2) {
+         if (map.containsKey(i)) {
+            System.out.println(i);
+            map.remove(i);
          }
       }
    }
