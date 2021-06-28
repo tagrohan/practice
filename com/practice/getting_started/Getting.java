13dbb0b307252de9451c40d779696c9ae1e68f4b
tagrohan <tagrohan@gmail.com>
2021-06-29 03:30:18 +0530

    rotateANumberV2 added

--- com/practice/getting_started/Getting.java
+++ com/practice/getting_started/Getting.java
@@ -2,11 +2,39 @@ package com.practice.getting_started;
 
 public class Getting {
    public static void main(String[] args) {
-      rotateANumber("12345", 2);
+      rotateANumberV2(12345, -2);
    }
+
+
+   // input is in int format
+   private static void rotateANumberV2(int num, int by) {
+//      rotateANumberV2(12345, -2);
+      int len = 0, temp = num;
+      while (temp > 0) {
+         len += 1;
+         temp = temp / 10;
+      }
+
+      by = (len + by) % len;
+      int first = 1, second = 1;
+      for (int i = 0; i < len; i++) {
+         if (i < by) {
+            first *= 10;
+         } else second *= 10;
+      }
+      System.out.println(second);
+      System.out.println(first);
+
+      second = (num % first) * second;
+      first = num / first;
+      System.out.println(second + first);
+
+   }
+
 
    // -2 anti - clock, 2 - clock
    private static void rotateANumber(String num, int by) {
+//      rotateANumber("12345", 2);
       int len = num.length();
       by = (len + by) % len;
       for (int i = len - by; i < len; i++) {
