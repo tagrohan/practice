c51a342803c3c8a418120613ee05d8bc09f08258
tagrohan <tagrohan@gmail.com>
2021-07-19 21:09:59 +0530

    BookSeatBlock bug fixed

--- com/practice/sync/BookSeatBlock.java
+++ com/practice/sync/BookSeatBlock.java
@@ -1,27 +1,10 @@
 package com.practice.sync;
 
-public class BookSeatBlock extends Thread {
-   private static int seatsLeft = 10;
-   private int seatsReq = 0;
 
-   @Override
-   public void run() {
-      bookSeat(seatsReq);
-   }
+class OrderSeats {
+   private int seatsLeft = 10;
 
-   public static void main(String[] args) {
-      BookSeatBlock seats1 = new BookSeatBlock();
-      BookSeatBlock seats2 = new BookSeatBlock();
-      seats1.seatsReq = 7;
-      seats2.seatsReq = 7;
-//      bookSeat(seats1.seatsReq);
-//      bookSeat(seats2.seatsReq);
-      seats1.start();
-      seats2.start();
-   }
-
-
-   private static void bookSeat(int seats) {
+   void bookSeat(int seats) {
       System.out.println("called via thread");
       synchronized (BookSeatBlock.class) {
          if (seatsLeft >= seats) {
@@ -33,4 +16,27 @@ public class BookSeatBlock extends Thread {
          }
       }
    }
+}
+
+public class BookSeatBlock extends Thread {
+   private int seatsReq;
+   static OrderSeats orderSeats;
+
+   @Override
+   public void run() {
+      orderSeats.bookSeat(seatsReq);
+   }
+
+   public static void main(String[] args) {
+      orderSeats = new OrderSeats();
+      BookSeatBlock seats1 = new BookSeatBlock();
+      BookSeatBlock seats2 = new BookSeatBlock();
+      seats1.seatsReq = 7;
+      seats2.seatsReq = 7;
+//      bookSeat(seats1.seatsReq);
+//      bookSeat(seats2.seatsReq);
+      seats1.start();
+      seats2.start();
+   }
+
 }
