090bf7be014c02cbe11ee7b9bb614bf1bbb7aa4f
tagrohan <tagrohan@gmail.com>
2021-08-30 14:01:43 +0530

    fact in ON added

--- trello/Recursion.java
+++ trello/Recursion.java
@@ -3,6 +3,14 @@ package trello;
 public class Recursion {
 
    public static void main(String[] args) {
-      System.out.println("recursion gonna start fro here from trello");
+      System.out.println(factorial(5));
+   }
+
+   private static int factorial(int value) {
+      int val = value;
+      if (value == 1) {
+         return value;
+      }
+      return val * factorial(value - 1);
    }
 }
