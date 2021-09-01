108530198b0f23a7d7bfbc488f70c12d7da7c274
tagrohan <tagrohan@gmail.com>
2021-09-01 23:33:02 +0530

    power in O(log n) and max and min in array

--- trello/Recursion.java
+++ trello/Recursion.java
@@ -3,10 +3,29 @@ package trello;
 public class Recursion {
 
    public static void main(String[] args) {
-      System.out.println(powerOn(2, 3));
+      System.out.println(powerOn2(2, 3));
+   }
+
+   private static int maxAndMin(int[] arr) {
+
+      return -1;
    }
 
 
+   // working with O(Log n)
+   private static int powerOn2(int value, int power) {
+      if (power <= 0) return 1;
+
+      int cVal = powerOn2(value, power / 2);
+      cVal *= cVal;
+      if (power % 2 == 1) {
+         cVal *= value;
+      }
+      return cVal;
+   }
+
+
+   // it's a O(n) we have O(log n as well) check up
    private static int powerOn(int value, int power) {
       if (power <= 0) return 1;
 
