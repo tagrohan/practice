87f4c1819bc6f9442a4160908a9ab54021c24640
tagrohan <tagrohan@gmail.com>
2022-01-08 13:41:43 +0530

    1-n rev

--- com/practice/Hello.java
+++ com/practice/Hello.java
@@ -2,6 +2,16 @@ package com.practice;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("hello world");
+        printRev(1 , 5);
+    }
+
+    // 5,4,3,2,1
+    private static void printRev(int start, int end) {
+
+        if (start > end) return;
+
+        System.out.println(end);
+        printRev(start, end - 1);
+
     }
 }
